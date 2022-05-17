4542166c9f557e28ac0810e04995d425892b53ba
Thomas Vedel <thomasvedel@hotmail.dk>
2022-05-17 11:52:21 +0200

    Preprocessing of digit data

--- Excercise1_knn.R
+++ Excercise1_knn.R
@@ -1,40 +1,97 @@
 install.packages("ggplot2")
+install.packages("glue")
 library(glue)       # used for string formatting
 library(class)      # contains KNN classifier
 library(ggplot2)    # for plotting and visualization
 install.packages("gmodels")
 library(gmodels)
+install.packages("spatstat")
+library(spatstat)
+install.packages("ggfortify")
+library(ggfortify)  
 
-# Data shuffle
-set.seed(423)
-ciphers_shuffle <- ciphers[sample(nrow(ciphers)),]
+#Pre-Processing
 
-# Data splittting
-
-split_ratio = 0.5
-samples <- sample.int(n = nrow(ciphers_shuffle), size = floor(split_ratio*nrow(ciphers_shuffle)))
-train <- ciphers_shuffle[samples, ]
-test  <- ciphers_shuffle[-samples, ]
-
-train_labels <- train[,2]
-test_labels <- test[,2]
-
-
-
-accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
-run_knn <- function(train, test, train_labels, k){
-  start_time <- Sys.time()
-  test_prediction <- knn(train,test,cl=train_labels,k)
-  end_time <- Sys.time()
-  confusion_matrix <- table(test_prediction, test_labels)
-  runtime = end_time - start_time
-  confusion_matrix
-  acc = accuracy(confusion_matrix)
-  cat("K:",k," Accuracy:",acc," Runtime:",runtime, "\n")
-  df <- list(K=k,Accuracy=acc,Runtime=runtime)
-  return(df)
+remove_constant <- function(dat, na.rm = FALSE, quiet=TRUE) {
+  mask <-
+    sapply(
+      X=seq_len(ncol(dat)),
+      FUN=function(idx) {
+        column_to_test <-
+          if (is.matrix(dat)) {
+            dat[, idx]
+          } else {
+            dat[[idx]]
+          }
+        length(unique(
+          if (na.rm) {
+            stats::na.omit(column_to_test)
+          } else {
+            column_to_test
+          }
+        )) <= 1 # the < is in case all values are NA with na.rm=TRUE
+      }
+    )
+  if (!quiet) {
+    remove_message(dat=dat, mask_keep=!mask, which="columns", reason="constant")
+  }
+  dat[ , !mask, drop=FALSE]
 }
 
-run_knn(train, test, train_labels, 3)
+digit_data <- remove_constant(ciphers[,3:786])
+dim(digit_data)
 
-run_knn(train, test, train_labels, 1)
+ciphers_processed <- matrix(0, nrow = dim(digit_data)[1], ncol = dim(digit_data)[2] + 2)
+ciphers_processed[,1:2] <- ciphers[,1:2]
+dim(ciphers_processed)
+ciphers_processed[,3:531] <- digit_data
+
+get_img <- function(img_data) {
+  x <- 1:23
+  y <- 1:23
+  img_data <- rev(img_data)
+  
+  img <- matrix(0, nrow = 23, ncol = 23)
+  for(i in x) { 
+    for (j in y) {                                  
+      img[i,j] <- img_data[j*23 - (i-1)]
+    }
+  }
+  img
+}
+
+# This function visualizes a given datapoint
+
+visualize_datapoint <- function(datapoint) {
+  img_data <- get_img(datapoint[3:531])
+  gt <- datapoint[2]
+  student_id <- datapoint[1]
+  
+  image(img_data, axes=FALSE, col=gray.colors(10), useRaster=TRUE)
+  title_string = "Ground truth: {gt} | Student id: {student_id}"
+  title(main=glue(title_string), col.main="blue", font.main=15)
+}
+
+par(mfrow=c(3,3))    # set the plotting area into a 3*3 array
+
+random_datpoint_dxs <- sample(1:66000, 9, replace=FALSE)
+
+for (i in random_datpoint_dxs) {
+  visualize_datapoint(ciphers_processed[i,])
+}
+
+gaussian_filter <- function(img, sig){
+  smoothed <- as.matrix(blur(as.im(img), sigma = sig, normalise=FALSE, bleed = TRUE, varcov=NULL))
+  return(smoothed)
+}
+
+img <- get_img(ciphers_processed[500,3:531])
+smt_img <- gaussian_filter(img, 1.3)
+
+image(img, axes=FALSE, col=gray.colors(10), useRaster=TRUE)
+title_string = "Original image"
+title(main=glue(title_string), col.main="blue", font.main=15)
+
+image(smt_img, axes=FALSE, col=gray.colors(10), useRaster=TRUE)
+title_string = "After applying Gaussian smoothing"
+title(main=glue(title_string), col.main="blue", font.main=25)
